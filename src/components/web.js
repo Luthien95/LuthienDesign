289742b1ca9875c74ca814d87b7d0cf098abdb2c
Aleksandra Salak <aleksandrasalak@gmail.com>
2018-12-15 00:10:04 +0100

    links repair

--- src/components/web.js
+++ src/components/web.js
@@ -14,57 +14,37 @@ class Web extends Component {
           <div className="o-website-description">
             <h3>My portfolio</h3>
             <p>website done in React</p>
-            <a
-              href="https://localhost:3000"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-angle-double-right" />
-            </a>
+            <i className="fas fa-angle-double-right" />
           </div>
+          <a href="https://localhost:3000" target="_blank" rel="noopener noreferrer"></a>
         </div>
-        <div className="c-projects__website">
-          <img src={FifthWebsite} alt="Design Website" href="https://luthien95.github.io/Design-Page/"
-              target="_blank"/>
+        <div className="c-projects__website" alt="Design Website" href="https://luthien95.github.io/Design-Page/"
+              target="_blank">
+          <img src={FifthWebsite} />
           <div className="o-website-description">
             <h3>Design & Design Website</h3>
             <p>website done in React</p>
-            <a
-              href="https://luthien95.github.io/Design-Page/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-angle-double-right" />
-            </a>
+            <i className="fas fa-angle-double-right" />
           </div>
+          <a href="https://luthien95.github.io/Design-Page/" target="_blank" rel="noopener noreferrer"></a>
         </div>
         <div className="c-projects__website">
           <img src={ThirdWebsite} alt="" />
           <div className="o-website-description">
             <h3>Bubbles</h3>
             <p>made as part of Canvas learning</p>
-            <a
-              href="https://codepen.io/Luthien95/full/eLOydd/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-angle-double-right" />
-            </a>
+            <i className="fas fa-angle-double-right" />
           </div>
+          <a href="https://codepen.io/Luthien95/full/eLOydd/" target="_blank" rel="noopener noreferrer"></a>
         </div>
         <div className="c-projects__website">
           <img src={SecondWebsite} alt="" />
           <div className="o-website-description">
             <h3>Company's website</h3>
             <p>project made in JS & Bootstrap</p>
-            <a
-              href="http://www.monar.wroclaw.pl/home.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-angle-double-right" />
-            </a>
+            <i className="fas fa-angle-double-right" />
           </div>
+          <a href="http://www.monar.wroclaw.pl/home.html" target="_blank" rel="noopener noreferrer"></a>
         </div>
       </div>
     );

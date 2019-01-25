550bd11d02d76586f02195a2c3b7f6725bcd1906
Aleksandra Salak <aleksandrasalak@gmail.com>
2019-01-25 22:39:10 +0100

    repair

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/main";
-import "./../style/css/index.css";
 
 class App extends Component {
   render() {
